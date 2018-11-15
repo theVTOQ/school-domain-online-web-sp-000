930772919496a79e881ec4b0c08416eeb0c6800f
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-15 00:41:27 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    ROSTER[:grade] = 
+  end
+end
