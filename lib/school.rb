c144f01d68eb280a2d86fcf6853ef8dee2e15bfd
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-15 00:52:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,10 +8,10 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster[:grade] == nil
-      @roster[:grade] = [name]
+    if @roster[grade] == nil
+      @roster[grade] = [name]
     else
-      @roster[:grade] << name
+      @roster[grade] << name
     end
   end
 
